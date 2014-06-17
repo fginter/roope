03d63815938ad799abab57c314440237145c6461
Jenna Kanerva <jmnybl@utu.fi>
2014-06-17 23:50:37 +0300

    Playing with the device. Fun! Fun! Fun! :)

--- jfbot.ino
+++ jfbot.ino
@@ -8,9 +8,47 @@
 /* This function runs only once, upon boot
    void is the return type of the function, or in this case it means that the function doesn't return a value
    */
-   
+int counter=0;   
 StepMotor left(513,7,8,9,10,11); //left motor, pins 9-12, indicator 13
 StepMotor right(513,3,4,5,6,12);    //right motor, pins 4-7, indicator 8
+
+// Move both tires simultaneously
+void move_both(long degs) {
+  long steps=(left.gearRatio*degs)/45L;
+  while (steps>0) {
+    left.singleStep(true);
+    right.singleStep(true);
+    steps--;
+    delayMicroseconds(left.stepDelay); // without this magnet is turning too fast?
+  }
+  left.deenergize(); // blinks led
+  right.deenergize();
+}
+
+
+void turn_left(long degs) {
+  long steps=(left.gearRatio*degs)/45L;
+  while (steps>0) {
+    left.singleStep(false);
+    right.singleStep(true);
+    steps--;
+    delayMicroseconds(left.stepDelay);
+  }
+  left.deenergize();
+  right.deenergize();
+}
+
+void turn_right(long degs) {
+  long steps=(left.gearRatio*degs)/45L;
+  while (steps>0) {
+    left.singleStep(true);
+    right.singleStep(false);
+    steps--;
+    delayMicroseconds(left.stepDelay);
+  }
+  left.deenergize();
+  right.deenergize();
+}
 
 void setup() {
   left.setSpeed(10);
@@ -22,8 +60,8 @@ void setup() {
   everything should happen.
   */
 void loop() {
-  left.singleStep(true);
-  right.singleStep(true);
-  delayMicroseconds(left.stepDelay);
-}
-
+  move_both(360*2);
+  delay(1000);
+  turn_left(360);
+  delay(1000);
+} 
