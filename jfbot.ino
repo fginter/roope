742f212288cbca702889465a568e191c0c3b6a90
Jenna Kanerva <jmnybl@utu.fi>
2014-06-19 00:27:45 +0300

    Just testing.

--- jfbot.ino
+++ jfbot.ino
@@ -8,7 +8,14 @@
 /* This function runs only once, upon boot
    void is the return type of the function, or in this case it means that the function doesn't return a value
    */
-int counter=0;   
+   
+/* Specs of our little prototype device:
+Axel: 94mm --> perimeter: 590.6mm
+Wheel diameter: 25mm --> perimeter: 78.5mm
+...1 degree is about 8*11 steps...
+*/
+   
+      
 StepMotor left(513,7,8,9,10,11); //left motor, pins 9-12, indicator 13
 StepMotor right(513,3,4,5,6,12);    //right motor, pins 4-7, indicator 8
 
@@ -27,7 +34,7 @@ void move_both(long degs) {
 
 
 void turn_left(long degs) {
-  long steps=(left.gearRatio*degs)/45L;
+  long steps=degs/2*8*11;
   while (steps>0) {
     left.singleStep(false);
     right.singleStep(true);
@@ -38,17 +45,6 @@ void turn_left(long degs) {
   right.deenergize();
 }
 
-void turn_right(long degs) {
-  long steps=(left.gearRatio*degs)/45L;
-  while (steps>0) {
-    left.singleStep(true);
-    right.singleStep(false);
-    steps--;
-    delayMicroseconds(left.stepDelay);
-  }
-  left.deenergize();
-  right.deenergize();
-}
 
 void setup() {
   left.setSpeed(10);
@@ -60,8 +56,8 @@ void setup() {
   everything should happen.
   */
 void loop() {
-  move_both(360*2);
+  move_both(360);
   delay(1000);
-  turn_left(360);
+  turn_left(90);
   delay(1000);
 } 
