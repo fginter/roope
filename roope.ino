d5d77a4ad321355cba26f5fe199c8a85cbed2d6a
Filip Ginter <ginter@cs.utu.fi>
2014-06-21 04:24:09 +0300

    ...well, you know...

--- roope.ino
+++ roope.ino
@@ -9,12 +9,14 @@
    void is the return type of the function, or in this case it means that the function doesn't return a value
    */
    
+int counter=0;
 StepMotor left(513,7,8,9,10,11); //left motor, pins 9-12, indicator 13
 StepMotor right(513,3,4,5,6,12);    //right motor, pins 4-7, indicator 8
 
 void setup() {
   left.setSpeed(10);
   right.setSpeed(10);
+  Serial.begin(9600);
 }
 
 /* The microcontroller just keeps running this function
