6166d3c69a4630746d725b3320ad9c5eed2d2f2e
Jenna Kanerva <jmnybl@utu.fi>
2014-07-27 00:20:48 +0300

    Skeleton of the connection class.

---
+++ connection.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include "connection.h"
+
+// Constructor
+Connection::Connection() {
+  // empty
+}
+
+void Connection::setup_connection() {
+  // TODO
+}
+
+char * Connection::fetch_command() {
+  // TODO
+  return &message;  
+}
---
+++ connection.h
@@ -0,0 +1,14 @@
+#ifndef _connection_h_
+#define _connection_h_
+
+
+class Connection {
+  
+  void setup_connection();
+  char* fetch_command(); 
+  
+};
+
+
+#endif
+
